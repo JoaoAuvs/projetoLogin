954bd4b8e25c5828df0625bce02b4f1de4dad4b7
JoaoAuvs <joaoauvs@gmail.com>
2021-04-16 12:25:16 -0300

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,40 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'dashboard',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'dashboard',
+    loadChildren: () => import('./pages/Authentication/dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/Authentication/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'registro',
+    loadChildren: () => import('./pages/Authentication/registro/registro.module').then( m => m.RegistroPageModule)
+  },
+  {
+    path: 'recuperacao-de-senha',
+    loadChildren: () => import('./pages/Authentication/recuperacao-de-senha/recuperacao-de-senha.module').then( m => m.RecuperacaoDeSenhaPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'tela-perfil',
+    loadChildren: () => import('./pages/tela-perfil/tela-perfil.module').then( m => m.TelaPerfilPageModule)
+  },
+  {
+    path: 'tela-embarcado',
+    loadChildren: () => import('./pages/tela-embarcado/tela-embarcado.module').then( m => m.TelaEmbarcadoPageModule)
+  },
+  {
+    path: 'tela-ambiente',
+    loadChildren: () => import('./pages/tela-ambiente/tela-ambiente.module').then( m => m.TelaAmbientePageModule)
   }
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,39 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+
+  usuario:string='Nome do Usuário'
+  email:string='email@gmail.com'
+
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Perfil', url: '/tela-perfil', icon: 'person-circle' },
+    { title: 'Ambientes', url: '/tela-ambiente', icon: 'pricetag' },
+    { title: 'Embarcados', url: '/tela-embarcado', icon: 'hardware-chip' },
+    { title: 'Logout', url: '/dashboard', icon: 'log-out' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  
+  /*public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];*/
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
 }
